c57cc0f259acfae34f782fd462e635ce0e9885f4
shisha999 <159075456+shisha999@users.noreply.github.com>
2025-10-16 22:44:16 -0400

    Mise à jour pxt.json, main.blocks, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

---
+++ tilemap.g.ts
@@ -0,0 +1,15 @@
+// Code généré automatiquement. Ne pas modifier.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "Hisoka":
+            case "tile1":return tile1;
+        }
+        return null;
+    })
+
+}
+// Code généré automatiquement. Ne pas modifier.
